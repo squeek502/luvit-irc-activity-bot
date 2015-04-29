4b4512c29d051f2cce69d99f0e9b620834c61fba
squeek <squeek502@hotmail.com>
2015-04-29 00:04:51 -0700

    Better package.lua  * See https://groups.google.com/d/msg/luvit/TcpBN47nrR8/eUHVIcWq_SoJ

--- package.lua
+++ package.lua
@@ -2,11 +2,14 @@ return {
   name = "squeek502/irc-activity-bot",
   version = "0.1.0",
   description = "irc bot that announces github activity",
+  keywords = {"irc", "bot", "github"},
+  homepage = "https://github.com/squeek502/luvit-irc-activity-bot",
   author = {
-    name="squeek"
+    name = "Ryan Liptak",
+    url = "http://www.ryanliptak.com"
   },
   dependencies = {
-    "luvit/luvit@2.1.0",
+    "luvit/luvit@2.1.1",
     "squeek502/irc@0.3.1",
     "squeek502/poller@0.2.1"
   },
